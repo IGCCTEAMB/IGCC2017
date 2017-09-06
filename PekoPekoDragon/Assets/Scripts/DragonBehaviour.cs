87d81b058ae9e0479c4b635dab77e1ffc2aee52e
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-09-06 17:59:18 +0900

    DragonBehaviour Script added

---
+++ PekoPekoDragon/Assets/Scripts/DragonBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DragonBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] waypoints;
+    [SerializeField]
+    private int num;
+    [SerializeField]
+    private float minDist;
+    [SerializeField]
+    private float speed;
+
+    private NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
+		
+        if(dist > minDist)
+        {
+            Move();
+        }
+        else
+        {
+            num = Random.Range(0, waypoints.Length);
+        }
+	}
+
+    void Move()
+    {
+        //gameObject.transform.LookAt(waypoints[num].transform.position);
+        //gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+        navMeshAgent.SetDestination(waypoints[num].transform.position);
+    }
+}
