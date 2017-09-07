f1047cb20667d95e34b8ba3e1043ada680601be9
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-09-07 13:45:19 +0900

    Dragon movement updated

---
+++ PekoPekoDragon/Assets/Scripts/Dragon/DragonBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DragonBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] waypoints;
+    [SerializeField]
+    private int num;
+    [SerializeField]
+    private float minDist;
+
+    [SerializeField]
+    private GameObject _targetObject;
+
+    private NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
+        _targetObject = gameObject.transform.GetChild(0).GetComponent<DragonDetector>().targetObject;
+
+        if (dist > minDist)
+        {
+            Move();
+        }
+        else
+        {
+            num = Random.Range(0, waypoints.Length);
+        }
+	}
+
+    void Move()
+    {
+        if(!_targetObject)
+        {
+            navMeshAgent.SetDestination(waypoints[num].transform.position);
+        }else{
+            navMeshAgent.SetDestination(_targetObject.transform.position);
+        }
+
+    }
+}
