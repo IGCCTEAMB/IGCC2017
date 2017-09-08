56358bcde9e45ccc6aa178497b1305a2176d83fd
Fuseya <2h.8p.5love.913@gmail.com>
2017-09-08 18:09:10 +0900

    Add GameManager

---
+++ PekoPekoDragon/Assets/Scripts/MoodController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoodController : MonoBehaviour
+{
+    bool isGood = true;
+    int frameCount = 0;
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		// 通常状態
+        if(isGood)
+        {
+
+        }
+        // 怒っている状態
+        else
+        {
+            // 自然と減らしていく
+            frameCount++;
+            if (frameCount > 20)
+            {
+
+            }
+        }
+    }
+}
