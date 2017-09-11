23ad22ce8cd461eb4b450e260f4c0cede589e4c1
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-09-11 13:36:50 +0900

    Updated dragon attack pattern

--- PekoPekoDragon/Assets/Scripts/Dragon/DragonBehaviour.cs
+++ PekoPekoDragon/Assets/Scripts/Dragon/DragonBehaviour.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DragonBehaviour : MonoBehaviour {
+public class DragonBehaviour : MonoBehaviour
+{
 
     [SerializeField]
     private GameObject[] waypoints;
@@ -21,14 +22,21 @@ public class DragonBehaviour : MonoBehaviour {
 
     private NavMeshAgent navMeshAgent;
 
-	// Use this for initialization
-	void Start () {
+    private float time;
+    public float attackDelay = 2;
+
+    // Use this for initialization
+    void Start()
+    {
         navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        time = 0;
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-       
+
+    // Update is called once per frame
+    void Update()
+    {
+
         float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
         _targetObject = gameObject.transform.GetChild(0).GetComponent<DragonDetector>().targetObject;
 
@@ -40,33 +48,48 @@ public class DragonBehaviour : MonoBehaviour {
         {
             num = Random.Range(0, waypoints.Length);
         }
-	}
+    }
 
 
     void Move()
     {
-        if(!_targetObject)
+        if (!_targetObject)
         {
             navMeshAgent.SetDestination(waypoints[num].transform.position);
-        }else{
+        }
+        else
+        {
 
             float dist = Vector3.Distance(gameObject.transform.position, _targetObject.transform.position);
 
             if (dist < _minDistWithPlayer)
             {
+
                 navMeshAgent.isStopped = true;
-                GameObject par = Instantiate(dragonAttacks[Random.Range(0, dragonAttacks.Length)],gameObject.transform.GetChild(1).transform.position,gameObject.transform.GetChild(1).transform.rotation);
+                Attack();
                 gameObject.transform.LookAt(_targetObject.transform.position);
-                
+
             }
             else
             {
-               navMeshAgent.isStopped = false;
+                navMeshAgent.isStopped = false;
 
-               navMeshAgent.SetDestination(_targetObject.transform.position);
+                navMeshAgent.SetDestination(_targetObject.transform.position);
             }
 
         }
 
+    }
+
+    void Attack()
+    {
+        time++;
+        int attackNum = Random.Range(0, dragonAttacks.Length);
+        if (time > attackDelay * 60.0f)
+        {
+            time = 0;
+            GameObject par = Instantiate(dragonAttacks[attackNum], gameObject.transform.GetChild(attackNum + 1).transform.position, gameObject.transform.GetChild(attackNum + 1).transform.rotation);
+            
+        }
     }
 }
