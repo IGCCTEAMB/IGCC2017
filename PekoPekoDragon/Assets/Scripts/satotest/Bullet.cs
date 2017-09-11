668c2ea6cdcafab583fb90fe10d93ae995a7b4d6
sato <mokoroshi@gmail.com>
2017-09-11 16:59:29 +0900

    player update

---
+++ PekoPekoDragon/Assets/Scripts/satotest/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    //消えるまでの時間
+    public float deletetime = 60;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間減らす
+        deletetime--;
+
+        //消す
+        if (deletetime == 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
