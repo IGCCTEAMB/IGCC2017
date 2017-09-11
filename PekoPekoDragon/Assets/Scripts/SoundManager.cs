311e3b8899d88de6784df79892a1ffb04ceff82e
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-09-11 17:28:29 +0900

    Add SoundManager

---
+++ PekoPekoDragon/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource sfx;
+    public AudioSource bgm;
+    public static SoundManager instance = null;
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfx.clip = clip;
+        sfx.Play();
+
+    }
+    public void PlayBGM(AudioClip clip)
+    {
+        bgm.clip = clip;
+        bgm.Play();
+    }
+}
