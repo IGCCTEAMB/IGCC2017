ffb519004ffddcea50c72bbbc52918f5e11e6bf2
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-09-14 00:23:31 +0900

    TitleScene DONE!!!

---
+++ PekoPekoDragon/Assets/Scripts/TitleCameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] waypoints;
+    [SerializeField]
+    private int num;
+    [SerializeField]
+    private float minDist;
+    [SerializeField]
+    private float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
+
+        if (dist > minDist)
+        {
+            Move();
+        }
+        else
+        {
+            num = Random.Range(0, waypoints.Length);
+        }
+
+    }
+    void Move()
+    {
+
+        gameObject.transform.LookAt(waypoints[num].transform.position);
+        gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+
+    }
+}
---
+++ PekoPekoDragon/Assets/Scripts/TitleCursorScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleCursorScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _playButton;
+    [SerializeField]
+    private GameObject _quitButton;
+
+    [SerializeField]
+    private Sprite[] _playSprites;
+
+    [SerializeField]
+    private Sprite[] _quitSprites;
+
+    private bool _playflag = true;
+
+    // Use this for initialization
+    void Start () {
+        _playButton.GetComponent<Image>().sprite = _playSprites[1];
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKey(KeyCode.RightArrow))
+        {
+            if(_playflag)
+            {
+                _playflag = false;
+                _playButton.GetComponent<Image>().sprite = _playSprites[0];
+                _quitButton.GetComponent<Image>().sprite = _quitSprites[1];
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (!_playflag)
+            {
+                _playflag = true;
+                _playButton.GetComponent<Image>().sprite = _playSprites[1];
+                _quitButton.GetComponent<Image>().sprite = _quitSprites[0];
+            }
+        }
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.KeypadEnter))
+        {
+            if (_playflag) SceneManager.LoadScene(1);
+            else Application.Quit();
+        }
+	}
+}
